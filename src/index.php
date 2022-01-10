894b3d254761bc8dcb65ce1d3435032edb2c4f39
bbulone <benja.bulone@gmail.com>
2022-01-10 17:14:20 +0100

    formulaire

--- src/index.php
+++ src/index.php
@@ -1,7 +1,19 @@
+
+<link rel="stylesheet" href="css/style.css">
 <?php
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 echo '<p>update : 2 déc. 2021 à 16h31 testtt  </p>';
-phpinfo();
+
+
+
 ?>
 
+<div class="monformulaire">
+    <h2>Ceci est un beau formulaire</h2>
+    <form action="">
+        <input type="text">
+        <button type="submit"></button>
+    </form>
+
+</div>
